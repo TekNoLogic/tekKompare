8370ffd441ff0f57e20b7b33d56e27f782ae8f21
Tekkub Stoutwrithe <tekkub@gmail.com>
2007-02-04 05:14:07 +0000

    tekKompare - ah yes, something else to rip apart and clean up... joys.
    git-svn-id: http://tekkub-wow.googlecode.com/svn/trunk/tekKompare@235 86fe6d9a-1522-0410-a387-bf9db416f0a0

---
+++ localization.lua
@@ -0,0 +1,17 @@
+﻿--[[ $Id: localization.lua 16607 2006-11-11 02:05:06Z fenlis $ ]]--
+
+INVTYPE_GUN="Gun"
+INVTYPE_CROSSBOW="Crossbow"
+INVTYPE_WAND="Wand"
+INVTYPE_THROWN="Thrown"
+INVTYPE_GUNPROJECTILE="Projectile"
+INVTYPE_BOWPROJECTILE="Projectile"
+
+EQCompare_KEY_DESC="Only display compare tooltip while holding custom key."
+EQCompare_RESET_DESC="Reset All settings."
+EQCompare_RESET="All settings are resetted."
+EQCompare_HOVERLINK_DESC="Enable display tooltip while hovering hyperlink in ChatFrame."
+EQCompare_HOVERLINK_SAFE_DESC="Use safe mode to display hovering hyperlink."
+EQCompare_SKIN_DESC="Turn off oSkin/Skinner tooltip texture."
+EQCompare_MERCHANTAUCTIONKEY_DESC="Enable Merchant/Auction comparision holding key."
+EQCompare_HOVERLINKKEY_DESC="Enable Hoverlink comparision holding key."
