5b2a40e7f62d66b9dfdb815fa190dd66a5b4eaf0
Tekkub Stoutwrithe <tekkub@gmail.com>
2007-02-10 01:38:47 +0000

    Moving HoverTip to integrate into tK
    git-svn-id: http://tekkub-wow.googlecode.com/svn/trunk/tekKompare@263 86fe6d9a-1522-0410-a387-bf9db416f0a0

---
+++ ChatHoverTips.lua
@@ -0,0 +1,30 @@
+
+local orig1, orig2 = {}, {}
+local GameTooltip = GameTooltip
+
+
+local function OnHyperlinkEnter(frame, link, ...)
+	if string.find(link, "^item") or string.find(link, "^enchant") then
+		GameTooltip:SetOwner(frame, "ANCHOR_TOPRIGHT")
+		GameTooltip:SetHyperlink(link)
+		GameTooltip:Show()
+	end
+
+	if orig1[frame] then return orig1[frame](frame, link, ...) end
+end
+
+local function OnHyperlinkLeave(frame, ...)
+	GameTooltip:Hide()
+	if orig2[frame] then return orig2[frame](frame, ...) end
+end
+
+
+local _G = getfenv(0)
+for i=1, NUM_CHAT_WINDOWS do
+	local frame = _G["ChatFrame"..i]
+	orig1[frame] = frame:GetScript("OnHyperlinkEnter")
+	frame:SetScript("OnHyperlinkEnter", OnHyperlinkEnter)
+
+	orig2[frame] = frame:GetScript("OnHyperlinkLeave")
+	frame:SetScript("OnHyperlinkLeave", OnHyperlinkLeave)
+end
